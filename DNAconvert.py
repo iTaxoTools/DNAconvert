23b962498c51b3a55431667fcd0a698c08f83e55
Vladimir Kharchev <necrosovereign@gmail.com>
2020-06-12 14:18:01 +0200

    Added command-line arguments parsing

--- DNAconvert.py
+++ DNAconvert.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
+import argparse
 import sys
 
-for arg in sys.argv[1:]:
-    print(arg)
+parser = argparse.ArgumentParser(description="Converts between file formats with genetic information")
+parser.add_argument('--informat', help="format of the input file")
+parser.add_argument('--outformat', help="format of the output file")
+parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), help="the input file")
+parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help="the output file")
+
+args=parser.parse_args()
+print(vars(args))
