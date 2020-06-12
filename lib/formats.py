88908bb81670fd8670b15c61985ff9c3b4044fd5
Vladimir Kharchev <necrosovereign@gmail.com>
2020-06-12 15:02:20 +0200

    Parsing formats and extensions

---
+++ lib/formats.py
@@ -0,0 +1,11 @@
+import tabfile
+
+formats = dict(
+        tab = tabfile.Tabfile
+        )
+
+extensions = dict(
+        tab = tabfile.Tabfile,
+        txt = tabfile.Tabfile,
+        tsv = tabfile.Tabfile
+        )
