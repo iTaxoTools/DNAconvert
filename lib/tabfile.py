iTaxoTools/DNAconvert

commit 80abdbdf0bcbb424123c9241a670a8adb428d569

Added comments

lib/tabfile.py
@@ -1,6 +1,6 @@
-from lib.record import *
-from lib.utils import *
 from typing import TextIO, List, Tuple, Callable, Iterator, Generator
+from lib.utils import *
+rom lib.record import *
 
 
 class Tabfile:
@@ -8,6 +8,9 @@ class Tabfile:
 
     @staticmethod
     def write(file: TextIO, fields: List[str]) -> Generator:
+        """
+        the writer method for tab format
+        """
         if 'seqid' not in fields:
             raise ValueError("Tab format expects a 'seqid'")
 
@@ -30,6 +33,9 @@ class Tabfile:
 
     @staticmethod
     def read(file: TextIO) -> Tuple[List[str], Callable[[], Iterator[Record]]]:
+        """
+        the reader method for tab format
+        """
         # read the heading for the list of fields
         fields = file.readline().rstrip('\n').split('\t')
         fields = list(map(str.casefold, fields))

.gitignore
@@ -1,3 +1,4 @@
 __pycache__
 testfiles/
 references/
+.vim/

DNAconvert.py
@@ -12,51 +12,97 @@ import gzip
 import lib.fasta
 from typing import Tuple, Type, Optional, TextIO, cast
 
-# splits two component extension
-
 
 def splitext(name: str) -> Tuple[str, str]:
+    """
+    Returns the two-part extension and the one-part extension of a file name
+
+    Example:
+        splitext("abc.fas.gb.gz") == (".gb.gz", ".gz")
+    """
     name, ext2 = os.path.splitext(name)
     _, ext1 = os.path.splitext(name)
     return (ext1 + ext2, ext2)
 
 
 def parse_format(name: Optional[str], ext_pair: Tuple[str, str]) -> Optional[Type]:
+    """
+    Lookups the format class based on the format name or the extensions.
+
+    Only checks the extensions, if the name is None.
+    Two-part extension has the priority
+
+    Examples:
+        parse_format("fasta", ext_pair) == lib.fasta.Fastafile
+
+        parse_format(None, (".hapv.fas", ".fas") == lib.fasta.HapviewFastafile
+
+        parse_format(None, (".txt.fas", ".fas") == lib.fasta.Fastafile
+    """
+    # destruct the ext_pair
     d_ext = ext_pair[0]
     ext = ext_pair[1]
+
     try:
+        # lookup the format name, or the two-part extension, if it doesn't exist
         return lib.formats.formats[name] if name else lib.formats.extensions[d_ext]
     except KeyError:
+        # both the format name and the two-part extension are unknown
         try:
+            # lookup the one-part extension
             return lib.formats.extensions[ext]
         except KeyError:
+            # all the lookups failed
             return None
 
 
 def convertDNA(infile: TextIO, outfile: TextIO, informat: Type, outformat: Type, **options: bool) -> None:
+    """
+    Converts infile of format informat to outfile of format outformat with given options
+
+    Possible options:
+        allow_empty_sequnces: if set, the records with empty sequences are also recorded in the outfile.
+           By default, records with empty sequences are discarded
+    """
+    # take a shortcut for convertion FastQ into FASTA
     if informat is lib.fasta.FastQFile and outformat is lib.fasta.Fastafile:
         lib.fasta.FastQFile.to_fasta(infile, outfile)
         return
 
+    # initialize reading the file
     fields, records = informat.read(infile)
 
+    # start the writer
     writer = outformat.write(outfile, fields)
     next(writer)
 
+    # keep track of the number of skipped records
     skipped = 0
+    # iterate over the records in infile
     for record in records():
+        # when 'allow_empty_sequences' is set, the condition is always true and the record is passed to the writer
+        # otherwise only the records with non-empty sequences are passed
         if record['sequence'] or options['allow_empty_sequences']:
             writer.send(record)
         else:
             skipped += 1
 
+    # finish the writing
+    writer.close()
+
+    # inform the user about the number of skipped records
     if skipped > 0:
         warnings.warn(
             f"{skipped} records did not contain a sequence and are therefore not included in the converted file.\n If you would like to keep the empty sequences, check 'Allow empty sequences' or pass the option '- -allow_empty_sequences")
-    writer.close()
 
 
 def convert_wrapper(infile_path: str, outfile_path: str, informat_name: str, outformat_name: str, **options: bool) -> None:
+    """
+    This the wrapper for convertDNA. It parses the arguments and deals with the errors.
+
+    Detects formats based on informat_name, outformat_name and extensions
+    Passes options to the convertDNA
+    """
     # detect extensions
     in_ext = splitext(infile_path)
     out_ext = splitext(outfile_path)
@@ -77,6 +123,7 @@ def convert_wrapper(infile_path: str, outfile_path: str, informat_name: str, out
 
     # open the input file
     if in_ext[1] == ".gz":
+        # if the input file is a gz archive, unpack it
         infile: TextIO = cast(TextIO, gzip.open(infile_path, mode='rt'))
     else:
         infile = open(infile_path)
@@ -88,6 +135,11 @@ def convert_wrapper(infile_path: str, outfile_path: str, informat_name: str, out
 
 
 def launch_gui() -> None:
+    """
+    This function runs the graphical interface
+
+    It is used when DNAconvert is launched without arguments
+    """
     # create window
     root = tk.Tk()
     root.title("DNAconvert")
@@ -132,13 +184,17 @@ def launch_gui() -> None:
     # command for the convert button
     def gui_convert() -> None:
         try:
+            # catch all warnings
             with warnings.catch_warnings(record=True) as warns:
                 convert_wrapper(infile_name.get(), outfile_name.get(),
                                 informat.get(), outformat.get(), allow_empty_sequences=allow_empty_sequences.get())
+                # display the warnings generated during the conversion
                 for w in warns:
                     tkinter.messagebox.showwarning("Warning", str(w.message))
+            # notify the user that the converions is finished
             tkinter.messagebox.showinfo(
                 "Done.", "The conversion has been completed")
+        # show the ValueErrors and FileNotFoundErrors
         except ValueError as ex:
             tkinter.messagebox.showerror("Error", str(ex))
         except FileNotFoundError as ex:
@@ -184,10 +240,11 @@ parser.add_argument(
     '--cmd', help="activates the command-line interface", action='store_true')
 parser.add_argument('--allow_empty_sequences', action='store_true',
                     help="set this to keep the empty sequences in the output file")
-parser.add_argument('--informat', help="format of the input file")
-parser.add_argument('--outformat', help="format of the output file")
-parser.add_argument('infile', nargs='?', help="the input file")
-parser.add_argument('outfile', nargs='?', help="the output file")
+parser.add_argument('--informat', default="", help="format of the input file")
+parser.add_argument('--outformat', default="",
+                    help="format of the output file")
+parser.add_argument('infile', default="", nargs='?', help="the input file")
+parser.add_argument('outfile', default="", nargs='?', help="the output file")
 
 # parse the arguments
 args = parser.parse_args()
@@ -196,12 +253,17 @@ args = parser.parse_args()
 if not args.cmd:
     launch_gui()
 else:
+    # launch in the command-line mode
     try:
+        # catch the warnging
         with warnings.catch_warnings(record=True) as warns:
             convert_wrapper(args.infile, args.outfile,
                             args.informat, args.outformat, allow_empty_sequences=args.allow_empty_sequences)
+
+            # display the warnings generated during the conversion
             for w in warns:
                 print(w.message)
+    # show the ValueErrors and FileNotFoundErrors
     except ValueError as ex:
         sys.exit(ex)
     except FileNotFoundError as ex:

lib/fasta.py
@@ -5,9 +5,10 @@ from lib.utils import *
 from typing import TextIO, Iterator, List, Generator, Tuple, Set
 
 
-# Yields a list of lines that comprise one FASTA record
 def split_file(file: TextIO) -> Iterator[List[str]]:
-
+    """
+    Returns iterator that yield records as lists of lines
+    """
     # find the beginning of the first record
     line = " "
     while line[0] != '>':
@@ -15,6 +16,7 @@ def split_file(file: TextIO) -> Iterator[List[str]]:
 
     # chunk contains the already read lines of the current record
     chunk = []
+    # put the first line of the first record into chunk
     chunk.append(line.rstrip())
 
     for line in file:
@@ -27,6 +29,7 @@ def split_file(file: TextIO) -> Iterator[List[str]]:
             yield chunk
             chunk = []
 
+        # put the first line of the new record into chunk
         chunk.append(line.rstrip())
 
     # yield the last record
@@ -34,10 +37,15 @@ def split_file(file: TextIO) -> Iterator[List[str]]:
 
 
 class Fastafile:
+    """ Class for standard FASTA files"""
+
     @staticmethod
     def write(file: TextIO, fields: List[str]) -> Generator:
+        """FASTA writer method"""
+        # the standard NameAssembler
         name_assembler = NameAssembler(fields)
 
+        # the writing loop
         while True:
             # receive a record
             try:
@@ -53,6 +61,8 @@ class Fastafile:
 
     @staticmethod
     def read(file: TextIO) -> Tuple[List[str], Callable[[], Iterator[Record]]]:
+        """FASTA reader method"""
+
         # FASTA always have the same fields
         fields = ['seqid', 'sequence']
 
@@ -65,32 +75,54 @@ class Fastafile:
 
 
 class UnicifierSN(Unicifier):
+    """
+    Unicifier specialized to generate species names for Hapview
+
+    It appends numbers without a separation character
+    """
+
     def __init__(self, length_limit: Optional[int] = None):
         super().__init__(length_limit)
         self._sep = ""
 
 
 class SpeciesNamer:
-    """makes correspondence between long species name in the record and unique short names
+    """
+    Makes correspondence between long species name in the record and unique short names
+
+    name(record: Record) -> str
+        returns Hapview short species name for the given record
     """
 
     def __init__(self, species: Set[str], species_field: Optional[str]):
+        """
+        species is the set of species' names in the input file.
+        species_field is the name of the field that contains the species' name
+        """
+        # if the species' name is not given
+        # self.name returns consecutive numbers
         if not species_field:
             self._count = 0
             self.name = self._count_name
             return
-        # make list of pair of species and first 4 letters of the second part of the name
 
         def short_name(name: str) -> str:
+            """Takes a binomial name and return the first 4 letters of the species part"""
             _, second_part = re.split(r'[ _]', name, maxsplit=1)
             try:
                 return second_part[0:4]
             except IndexError:
                 raise ValueError(f"Malformed species name {name}")
+
+        # make Unicifier for the short names
         unicifier = UnicifierSN()
+
+        # save the field name
         self._species_field = species_field
+        # generate a dictionary from the binomial name to the unique short name
         self._species = {long_name: unicifier.unique(
             short_name(long_name)) for long_name in species}
+        # self.name does the lookup in the above dictionary
         self.name = self._dict_name
 
     def _count_name(self, record: Record) -> str:
@@ -102,8 +134,15 @@ class SpeciesNamer:
 
 
 class HapviewFastafile:
+    """class for the FASTA format of the Haplotype Viewer"""
+
     @ staticmethod
     def read(file: TextIO) -> Tuple[List[str], Callable[[], Iterator[Record]]]:
+        """
+        FASTA Hapview reader method
+
+        The same as for the standard FASTA     
+        """
         # FASTA always have the same fields
         fields = ['seqid', 'sequence']
 
@@ -116,6 +155,10 @@ class HapviewFastafile:
 
     @ staticmethod
     def write(file: TextIO, fields: List[str]) -> Generator:
+        """FASTA Hapview writer method"""
+        # if there is a field with the name of the species
+        # then aggregate the names into a set
+        # else use the standard Phylip Aggregator
         species_field = get_species_field(fields)
         if species_field:
             def species_reducer(acc: Set[str], record: Record) -> Set[str]:
@@ -125,8 +168,9 @@ class HapviewFastafile:
             aggregator = PhylipAggregator((set(), species_reducer))
         else:
             aggregator = PhylipAggregator()
-        records = []
 
+        # collect the record and aggregate the information about them
+        records = []
         while True:
             try:
                 record = yield
@@ -134,14 +178,18 @@ class HapviewFastafile:
                 break
             aggregator.send(record)
             records.append(record)
-
         [max_length, min_length, species] = aggregator.results()
-        species_namer = SpeciesNamer(species, species_field)
 
+        # will create the short species' names
+        species_namer = SpeciesNamer(species, species_field)
+        # will ensure that all sequences have the same length
         aligner = dna_aligner(max_length, min_length)
+        # creates or copies the seqid
         name_assembler = NameAssembler(fields)
+        # makes the seqid unique
         unicifier = Unicifier(100)
 
+        # write the records
         for record in records:
             print('>', unicifier.unique(name_assembler.name(record)),
                   '.', species_namer.name(record), sep="", file=file)
@@ -149,22 +197,31 @@ class HapviewFastafile:
 
 
 class FastQFile:
+    """class for the FastQ format"""
 
     @ staticmethod
     def to_fasta(infile: TextIO, outfile: TextIO) -> None:
+        """Quick conversion from FastQ to FASTA"""
         for line in infile:
+            # loop through lines until the start of a record
             if line[0] == '@':
+                # copy the seqid
                 print('>', line[1:], sep="", end="", file=outfile)
+                # copy the sequence
                 line = infile.readline()
                 print(line, file=outfile, end="")
 
     @ staticmethod
     def read(file: TextIO) -> Tuple[List[str], Callable[[], Iterator[Record]]]:
+        """FastQ reader method"""
+        # FastQ always have the same fields
         fields = ['seqid', 'sequence',
                   'quality_score_identifier', 'quality_score']
 
         def record_generator() -> Iterator[Record]:
             for line in file:
+                # loop until the start of a record
+                # then read 4 lines and yield them as a record
                 if line[0] == '@':
                     seqid = line[1:].rstrip()
                     sequence = file.readline().rstrip()
@@ -175,20 +232,32 @@ class FastQFile:
 
     @ staticmethod
     def write(file: TextIO, fields: List[str]) -> Generator:
+        """FastQ writer method"""
+
+        # check that all the required fields are present
         if not {'seqid', 'sequence', 'quality_score_identifier', 'quality_score'} <= set(fields):
             raise ValueError(
                 'FastQ requires the fields seqid, sequence, quality_score_identifier and quality_score')
+
         while True:
+            # get the record
             try:
                 record = yield
             except GeneratorExit:
                 break
+            # write the name
             print('@', record['seqid'], sep="", file=file)
+            # write the other attributes
             for field in ['sequence', 'quality_score_identifier', 'quality_score']:
                 print(record[field], file=file)
 
 
 class NameAssemblerGB(NameAssembler):
+    """
+    A specialization of NameAssembler of Genbank FASTA. 
+
+    It gives the higher priority to copying the seqid. Otherwise it is assembled from the organism and specimen_voucher field"""
+
     def __init__(self, fields: List[str]):
         if 'seqid' in fields:
             self.name = self._simple_name
@@ -199,42 +268,69 @@ class NameAssemblerGB(NameAssembler):
 
 
 class GenbankFastaFile:
+    """class for the Genbank FASTA submission format"""
+
+    # the list of Genbank fields
     genbankfields = ['seqid', 'organism', 'accession', 'specimen_voucher', 'strain', 'isolate', 'country', 'sequence', 'mol_type', 'altitude', 'bio_material', 'cell_line', 'cell_type', 'chromosome', 'citation', 'clone', 'clone_lib', 'collected_by', 'collection_date', 'cultivar', 'culture_collectiondb_xref', 'dev_stage', 'ecotype', 'environmental_samplefocus', 'germlinehaplogroup',
                      'haplotype', 'host', 'identified_by', 'isolation_source', 'lab_host', 'lat_lon', 'macronuclearmap', 'mating_type', 'metagenome_source', 'note', 'organelle', 'PCR_primersplasmid', 'pop_variant', 'proviralrearrangedsegment', 'serotype', 'serovar', 'sex', 'sub_clone', 'submitter_seqid', 'sub_species', 'sub_strain', 'tissue_lib', 'tissue_type', 'transgenictype_material', 'variety']
 
     @ staticmethod
     def prepare(fields: List[str], record: Record) -> None:
+        """
+        Transforms the record to the simple form.
+
+        Fuses country, region and locality fields,
+        replaces species field with the organism field,
+        remove uncertain bases from the beginning and the end of the sequence
+        """
+        # fuse country, region and locality
         if 'country' in fields:
             region = record.get('region')
             locality = record.get('locality')
             if region:
+                # "country: region[, locality]"
                 record['country'] = record['country'] + \
                     f": {region}" + (f", {locality}" if locality else "")
             else:
+                # "country[: locality]"
                 record['country'] = record['country'] + \
                     (f": {locality}" if locality else "")
+        # replace species field with organism field
         if 'organism' not in fields:
             try:
                 record['organism'] = record['species']
             except KeyError:
                 pass
+        # strip the sequence of uncertain bases
         record['sequence'] = record['sequence'].strip("nN?")
 
     @ staticmethod
     def parse_ident(line: str) -> Tuple[str, Dict[str, str]]:
+        """Reads the attributes from the first line of Genbank FASTA record. Returns seqid and the dictionary of attributes"""
+        # raise an error if the line is invalid
         if line[0] != '>':
             raise ValueError("Genbank fasta: invalid identifier line\n" + line)
+        # split out the seqid
         [seqid, values_str] = line[1:].split(maxsplit=1)
+
+        # collect the attributes
         values: Dict[str, str] = {}
-        for m in re.finditer(r'\[([^=\]]+)=([^\]]+)\]', values_str):
+        # the regex matches [field=value], field is stored in group 1, value in group 2
+        field_value_regex = r'\[([^=\]]+)=([^\]]+)\]'
+        for m in re.finditer(field_value_regex, values_str):
             field = m.group(1).strip()
             value = m.group(2).strip()
             if field == 'country':
+                # special treatment for the country field
+                # split into country, region, locality
                 place = re.split(r'[,:] ', value)
+                # put into the dictionary
                 values.update(
                     zip(['country', 'region', 'locality'], place + ['', '']))
             else:
                 values[field] = value
+
+        # initialise all the missing fields
         for field in GenbankFastaFile.genbankfields:
             if not (field == "seqid" or field == "sequence"):
                 values.setdefault(field, "")
@@ -242,36 +338,57 @@ class GenbankFastaFile:
 
     @ staticmethod
     def read(file: TextIO) -> Tuple[List[str], Callable[[], Iterator[Record]]]:
+        """Genbank FASTA reader method"""
         def record_generator() -> Iterator[Record]:
             for chunk in split_file(file):
                 ident = chunk[0]
+                # parse the seqid and attributes
                 seqid, values = GenbankFastaFile.parse_ident(ident)
                 yield Record(seqid=seqid, sequence="".join(chunk[1:]), **values)
         return GenbankFastaFile.genbankfields, record_generator
 
     @staticmethod
     def write(file: TextIO, fields: List[str]) -> Generator:
+        """Genbank FASTA writer method"""
+        # discard the invalid fields
         fields = [
             field for field in fields if field in GenbankFastaFile.genbankfields]
+        # raise a warning if the required fields are not present
         if not (('organism' in fields or 'species' in fields) and ('specimen_voucher' in fields or 'isolate' in fields or 'clone' in fields or 'haplotype' in fields)):
             warnings.warn("Your file has been converted. However, apparently in your tab file either the organism, or a unique source identifier (specimen-voucher, isolate, clone) was missing, which may be required for submission to GenBank")
+
+        # so far no sequence <200bp
         length_okay = True
+        # so far no sequences with dashes
         no_dashes = True
+
+        # creates seqid for Genbank FASTA
         name_assembler = NameAssemblerGB(fields)
+        # makes the seqid unique within 25 characters
         unicifier = Unicifier(25)
+
+        # receive the records and write them
         while True:
             try:
                 record = yield
             except GeneratorExit:
                 break
+
+            # standardize the record
             GenbankFastaFile.prepare(fields, record)
+
+            # raise the warning if the sequence <200 bp and turn off the checking for this
             if length_okay and len(record['sequence']) < 200:
                 length_okay = False
                 warnings.warn(
                     "Some of your sequences are <200 bp in length and therefore will probably not accepted by the GenBank nucleotide database")
+
+            # raise the warning if the sequence has dashes and turn off the checking for this
             if no_dashes and '-' in record['sequence']:
                 no_dashes = False
                 warnings.warn("Some of your sequences contain dashes (gaps) which is only allowed if you submit them as alignment. If you do not wish to submit your sequences as alignment, please remove the dashes before conversion.")
+            # print seqid and attributes
             print('>'+unicifier.unique(name_assembler.name(record)), *
                   [f"[{field}={record[field].strip()}]" for field in fields if not (field == "seqid" or field == "sequence")], file=file)
+            # print the sequence
             print(record['sequence'], file=file)

lib/formats.py
@@ -4,6 +4,7 @@ import lib.phylip as phylip
 import lib.nexus as nexus
 import lib.genbank as genbank
 
+# formats' names dictionary
 formats = dict(
     tab=tabfile.Tabfile,
     fasta=fasta.Fastafile,
@@ -16,6 +17,7 @@ formats = dict(
     genbank=genbank.GenbankFile,
 )
 
+# extensions' dictionary
 extensions = {
     ".tab": tabfile.Tabfile,
     ".txt": tabfile.Tabfile,

commit 230489a07c121a69ed7055fcbe33595a56afaafa

Fixed breaking typo introduced in the commit 80abdbdf0bcbb424123c9241a670a8adb428d569

lib/tabfile.py
@@ -1,6 +1,6 @@
 from typing import TextIO, List, Tuple, Callable, Iterator, Generator
 from lib.utils import *
-rom lib.record import *
+from lib.record import *
 
 
 class Tabfile: