2981306eab3487d110b379ac9009c1022c112122
Vladimir Kharchev <necrosovereign@gmail.com>
2021-05-07 15:04:00 +0200

    Processing class for the tab-no headers format

--- lib/tabfile.py
+++ lib/tabfile.py
@@ -74,3 +74,68 @@ class Tabfile:
 
         # return the list of fields and the generator closure
         return fields, record_generator
+
+
+class NoHeaderTab:
+    """Class for reading and writing tab-separated files without headers with genetic information"""
+
+    dna_characters = set("ATGCRYMKSWBDHVNUatgcrymkswbdhvnu-?")
+
+    @staticmethod
+    def is_sequence(s: str) -> bool:
+        return all(map(lambda c: c in NoHeaderTab.dna_characters, s))
+
+    @staticmethod
+    def write(file: TextIO, fields: List[str]) -> Generator:
+        """
+        the writer method for 'tab-no headers' format
+        """
+
+        while True:
+            # receive a record
+            try:
+                record = yield
+            except GeneratorExit:
+                break
+
+            # collect record fields in a list and join them with tabs
+            file.writelines('\t'.join([record[field]
+                                       for field in fields]) + '\n')
+
+    @staticmethod
+    def read(file: TextIO) -> Tuple[List[str], Callable[[], Iterator[Record]]]:
+        """
+        the reader method for 'tab-no headers' format
+        """
+        # always the same heading
+        fields = ['seqid', 'sequence']
+
+        # closure that will iterate over the subsequent lines and yield the records
+
+        def record_generator() -> Iterator[Record]:
+            sequence_index = -1
+            first_line = True
+
+            for line in file:
+                line = line.rstrip('\n')
+                # skip blank lines
+                if line.isspace() or line == "":
+                    continue
+                # split line into values
+                values = [value for value in line.split('\t') if value]
+
+                # Test if the first column is more probable to be a sequence
+                if first_line:
+                    if not NoHeaderTab.is_sequence(values[-1]) and NoHeaderTab.is_sequence(values[0]):
+                        sequence_index = 0
+                        warnings.warn(
+                            "The last column contains non-standard DNA characters. The first column is assumed to be the sequence column")
+                    first_line = False
+
+                sequence = values.pop(sequence_index)
+                seqid = "_".join(sanitize(value) for value in values)
+
+                yield Record(seqid=seqid, sequence=sequence)
+
+        # return the list of fields and the generator closure
+        return fields, record_generator
