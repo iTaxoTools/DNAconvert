3a949c96f4ab433e93516b02b583b2ce35c1d6a4
Vladimir Kharchev <necrosovereign@gmail.com>
2022-02-02 17:42:17 +0100

    writer method for NeXML

--- src/itaxotools/DNAconvert/library/nexml.py
+++ src/itaxotools/DNAconvert/library/nexml.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python3
 
-from typing import List, Tuple, Callable, Iterator, TextIO
+from typing import List, Tuple, Callable, Iterator, TextIO, Generator, Dict
 
 from dendropy import DnaCharacterMatrix
 
 from .record import Record
+from .utils import NameAssembler
 
 
 class NeXMLFile:
     """Class for NeXML files"""
 
     @staticmethod
+    def write(file: TextIO, fields: List[str]) -> Generator:
+        """NeXML writer method"""
+
+        name_assembler = NameAssembler(fields)
+        sequence_dict: Dict[str, str] = {}
+
+        while True:
+            try:
+                record = yield
+            except GeneratorExit:
+                break
+            sequence_dict[name_assembler.name(record)] = record["sequence"]
+
+        data = DnaCharacterMatrix.from_dict(sequence_dict)
+        data.write_to_stream(file, schema="nexml", markup_as_sequences=True)
+
+    @staticmethod
     def read(file: TextIO) -> Tuple[List[str], Callable[[], Iterator[Record]]]:
+        """NeXML reader method"""
 
         # NeXML always have the same fields
         fields = ["seqid", "sequence"]
